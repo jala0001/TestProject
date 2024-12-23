5cf31e4ba0f977a61e8676aba84bdaf9ee8884ce
jala0001 <jala0001@stud.kea.dk>
2024-12-23 16:24:34 +0100

    opdatere github

---
+++ src/main/java/com/example/testproject2/TestProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.testproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProject2Application.class, args);
+    }
+
+}
